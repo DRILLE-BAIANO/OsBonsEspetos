f53782cee62d617787760b8504cc018ca66ccd05
DRILLE-BAIANO <duduzimhard@gmail.com>
2025-11-25 12:26:37 -0300

    estilizamos o carrinho

---
+++ OsBonsEspetos/Migrations/20251125141537_AdicionarImagemUrlAoProduto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OsBonsEspetos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarImagemUrlAoProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
